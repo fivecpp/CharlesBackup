a8cc0b85989ddfaa0dfeb5e080897c2eac8e6085
Charles Xu <charl3s.xu@gmail.com>
2018-01-12 20:59:06 +0800

    added bookmarks

---
+++ bookmarks.fish
@@ -0,0 +1,9 @@
+#!/usr/bin/fish
+
+abbr tfnote 'cd ~/Workspace/own/Notes/Machine_Learning/TensorFlow/;ls'
+abbr mlnote 'cd ~/Workspace/own/Notes/Machine_Learning/;ls'
+
+abbr tfpy 'cd ~/Workspace/own/Python-Scripts/src/MachineLearning/TensorFlow/;ls'
+abbr mlpy 'cd ~/Workspace/own/Python-Scripts/src/MachineLearning/;ls'
+
+abbr prj 'cd ~/Workspace/official/drlff/;ls'
